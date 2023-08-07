9390b8b4a6179652f26fc9690256516e42ac212e
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2023-08-07 11:45:16 +0700

    Initial Commit

---
+++ ArtGems/ArtGemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGemsApp.swift
+//  ArtGems
+//
+//  Created by Thu Nguyen  on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ArtGemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
